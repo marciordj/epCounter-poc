402476a29f56b2374f092ec5509256c816acf069
Luiz Marcio de Jesus Rodrigues Junior <lmarciorodriguesjunior@gmail.com>
2022-05-31 19:37:54 -0300

    style: themes

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,5 +1,18 @@
 export default {
   colors: {
-    background: '#0F285E'
+    background: '#1c31a5'
   }
 };
+
+/*
+Paleta de cores:
+Backgrounds:
+  #1c31a5
+  #101f78
+  #020f59
+  #010937
+  #000524
+
+Text:
+ #FFF
+*/
